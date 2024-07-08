717c5cb64c4adb463693263d3816d99dcc56c4b5
MayankJatav <mayankjatav7898@gmail.com>
2024-07-08 12:27:24 +0530

    Added code for getting image and label from the dataset

--- dataloader.py
+++ dataloader.py
@@ -1,12 +1,19 @@
-import os
+import glob
 import torch.utils.data import Dataset
+from torchvision.io import read_image
 
 class FabricDataset(Dataset):
-    def __init__(self):
+    def __init__(self, path, transform=None):
         # Initialization Code Here
+        self.transform = transform
+        self.files = glob.glob(path+"\\**\\**\\*.png", recursive=True)
 
     def __len__(self):
         # Reuturn  the length
+        return len(self.files)
 
     def __getitem__(self, idx):
         # Return the data at index idx
+        image = read_image(self.files[idx])
+        label = self.files[idx].split('\\')[-3]
+        return image
