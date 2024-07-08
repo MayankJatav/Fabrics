8114c32e14f785b273998370f1ac5f520d22878d
MayankJatav <mayankjatav7898@gmail.com>
2024-07-08 18:49:47 +0530

    Implemented Use Of Utilities

--- dataloader.py
+++ dataloader.py
@@ -1,12 +1,15 @@
 import glob
+import torch
 from torch.utils.data import Dataset
 from torchvision.io import read_image
+from utils import Utilities as utils
 
 class FabricDataset(Dataset):
     def __init__(self, path, transform=None):
         # Initialization Code Here
         self.transform = transform
         self.files = glob.glob(path+"\\**\\**\\*.png", recursive=True)
+        self.utils = utils()
 
     def __len__(self):
         # Reuturn  the length
@@ -18,4 +21,5 @@ class FabricDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         label = self.files[idx].split('\\')[-3]
+        label = torch.tensor(self.utils.class_to_index(label))
         return image, label
