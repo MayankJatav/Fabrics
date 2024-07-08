50d2072530968206375cfc9ab8d9c363112feaf5
MayankJatav <mayankjatav7898@gmail.com>
2024-07-08 17:53:10 +0530

    Added Mobilenet V2 with Custom Classification Layer

--- model.py
+++ model.py
@@ -1,8 +1,23 @@
-import torch.nn as nn
+import torch
+import torchvision.models as models
 
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
+        self.mobilenet = models.mobilenet_v2(pretrained=True)
+        self.feature_extractor = self.mobilenet.features
+        self.classification = torch.nn.Sequential(
+            torch.nn.Linear(1280, 320),
+            torch.nn.ReLU(),
+            torch.nn.Linear(320, 80),
+            torch.nn.ReLU(),
+            torch.nn.Linear(80, 21),
+            torch.nn.Softmax(),
+        )
 
     def forward(self, input):
-        # TODO: implement forward pass
+        x = self.feature_extractor(input)
+        x = torch.nn.functional.adaptive_avg_pool2d(x, 1)
+        x = torch.flatten(x)
+        x = self.classification(x)
+        return x
