b344b543f448ded68b0e9a01a6f52bc87fed715c
MayankJatav <mayankjatav7898@gmail.com>
2024-07-08 13:56:03 +0530

    Added Running Device Code

--- trainer.py
+++ trainer.py
@@ -1,19 +1,31 @@
+import torch
 import torch.optim as optim
 from tqdm.auto import tqdm
 
 class Trainer:
 
-    def __init__(self, model, loss_fn, optimizer, epochs, train_loader, val_loader=None):
+    def __init__(self, model, loss_fn, optimizer, epochs, train_loader, val_loader=None, device=None):
         self.loss_fn = loss_fn
         self.optimizer: optim.Optimizer = optimizer
         self.epochs = epochs
         self.train_loader = train_loader
         self.val_loader = val_loader
         self.model = model
+        self.device = device
         self.train_loss = []
         self.val_loss = []
         self.train_acc = []
         self.val_acc = []
+
+        if device is None:
+                self.device = (
+                "cuda"
+                if torch.cuda.is_available()
+                else "mps"
+                if torch.backends.mps.is_available()
+                else "cpu"
+            )
+        print(f"Using {self.device} device")
 
     def train_one_epoch():
         print("Training")
@@ -22,6 +34,7 @@ class Trainer:
         model.train()
         for i, data in tqdm(enumerate(self.train_loader), total=len(self.train_loader)):
             inputs, labels = data
+            inputs, labels = inputs.to(self.device), labels.to(self.device)
             self.optimizer.zero_grad()
             outputs = self.model(inputs)
             loss = self.loss_fn(outputs, labels)
@@ -41,6 +54,7 @@ class Trainer:
         with torch.no_grad():
             for i, data in tqdm(enumerate(self.val_loader), total=len(self.val_loader)):
                 inputs, labels = data
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
                 outputs = self.model(inputs)
                 loss = self.loss_fn(outputs, labels)
                 val_running_loss += loss.item()
