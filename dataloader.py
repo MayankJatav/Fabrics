b4ea24d6cb748ffc0237cc9775eba48865a62ae8
MayankJatav <mayankjatav7898@gmail.com>
2024-07-08 12:27:25 +0530

    Added Transforms Code

--- dataloader.py
+++ dataloader.py
@@ -15,5 +15,7 @@ class FabricDataset(Dataset):
     def __getitem__(self, idx):
         # Return the data at index idx
         image = read_image(self.files[idx])
+        if self.transform:
+            image = self.transform(image)
         label = self.files[idx].split('\\')[-3]
         return image
