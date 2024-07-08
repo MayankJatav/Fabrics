20e54aa6d8419f8e6a54d5d3d9b5fe0ddb8637b1
MayankJatav <mayankjatav7898@gmail.com>
2024-07-08 12:27:24 +0530

    Added Initial Code For Dataloader

---
+++ dataloader.py
@@ -0,0 +1,12 @@
+import os
+import torch.utils.data import Dataset
+
+class FabricDataset(Dataset):
+    def __init__(self):
+        # Initialization Code Here
+
+    def __len__(self):
+        # Reuturn  the length
+
+    def __getitem__(self, idx):
+        # Return the data at index idx
