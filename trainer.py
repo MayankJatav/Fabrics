5bc42d2dd2b761988a717a1b10dbaa6e86736fed
MayankJatav <mayankjatav7898@gmail.com>
2024-07-08 12:27:25 +0530

    Added The Validation Code

--- trainer.py
+++ trainer.py
@@ -10,34 +10,48 @@ class Trainer:
         self.train_loader = train_loader
         self.val_loader = val_loader
         self.model = model
-        self.loss = 0.0
-        self.correct = 0
         self.train_loss = []
         self.val_loss = []
         self.train_acc = []
         self.val_acc = []
 
     def train_one_epoch():
-        self.loss = 0.0
-        self.correct = 0
+        print("Training")
+        train_running_loss = 0.0
+        train_running_correct = 0
+        model.train()
         for i, data in tqdm(enumerate(self.train_loader), total=len(self.train_loader)):
             inputs, labels = data
             self.optimizer.zero_grad()
-            outputs = model(inputs)
-            self.loss = self.loss_fn(outputs, labels)
-            self.loss.backward()
+            outputs = self.model(inputs)
+            loss = self.loss_fn(outputs, labels)
+            train_running_loss += loss.item()
+            train_running_correct = (outputs.argmax(1) == labels).sum().item()
+            loss.backward()
             self.optimizer.step()
-            self.loss += self.loss.item()
-            self.correct = (outputs.argmax(1) == labels).sum().item()
-        self.loss /= len(train_loader)
-        acc = 100 * (self.correct / len(train_loader))
-        return self.loss, acc
+        epoch_loss = train_running_loss / len(train_loader)
+        epoch_acc = 100 * (train_running_correct / len(train_loader))
+        return epoch_loss, epoch_acc
+
+    def validate_one_epoch():
+        print("Validation")
+        model.eval()
+        val_running_loss = 0.0
+        val_running_correct = 0
+        with torch.no_grad():
+            for i, data in tqdm(enumerate(self.val_loader), total=len(self.val_loader)):
+                inputs, labels = data
+                outputs = self.model(inputs)
+                loss = self.loss_fn(outputs, labels)
+                val_running_loss += loss.item()
+                val_running_correct = (outputs.argmax(1) == labels).sum().item()
+        epoch_loss = val_running_loss / len(val_loader)
+        epoch_acc = 100 * (val_running_correct / len(val_loader))
+        return epoch_loss, epoch_acc
 
     def train():
         for epoch in range(self.epochs):
-            model.train()
             train_epoch_loss, train_epoch_acc = train_one_epoch()
-            model.eval()
             val_epoch_loss, val_epoch_acc = validate_one_epoch()
             self.train_loss.append(train_epoch_loss)
             self.train_acc.append(train_epoch_acc)
@@ -45,3 +59,5 @@ class Trainer:
             self.val_acc.append(val_epoch_acc)
             print(f"Training loss: {train_epoch_loss:.3f}, Training acc: {train_epoch_acc:.3f}")
             print(f"Validation loss: {val_epoch_loss:.3f}, Validation acc: {val_epoch_acc:.3f}")
+            print()
+        print("Training Complete")
