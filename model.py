8865aa54e3d00feed4d6537d4671ac2bcaed3211
MayankJatav <mayankjatav7898@gmail.com>
2024-07-08 12:27:25 +0530

    Added Code for Model

---
+++ model.py
@@ -0,0 +1,8 @@
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+    def forward(self, input):
+        # TODO: implement forward pass
