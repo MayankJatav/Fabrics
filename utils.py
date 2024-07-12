b0cf93bd23766133e14613695fcef7c18bf4266b
MayankJatav <mayankjatav7898@gmail.com>
2024-07-12 17:38:01 +0530

    Modified Utils So That The Functions Can Be Used Without Creating The Object Of The Class And Added Utility For Checking Whether The Given String Is A Number Or Not

--- utils.py
+++ utils.py
@@ -1,17 +1,26 @@
 import json
 
+classes = ['Acetate', 'Acrylic', 'Angora', 'Cashmere', 'Chenille cotton', 'Cotton', 'Dralon', 'Elastane', 'Erion', 'Fleece', 'LI', 'Leather', 'Linen', 'Lut', 'Lycra', 'Metallic fibers', 'Micropoly', 'Modal', 'Naylon', 'Nylon', 'PES', 'Polyamide', 'Polyester', 'RAY', 'Rayon', 'Repreve', 'SP', 'Saduk Silk', 'Satin Silk', 'Silk', 'Spandex', 'Supplex Nylon', 'Synthetic', 'Viscose', 'Wool']
+
 class Utilities:
 
-    def __init__(self):
-        self.classes = ['Acrylic', 'Blended', 'Chenille', 'Corduroy', 'Cotton', 'Crepe', 'Denim', 'Felt', 'Fleece', 'Leather', 'Linen', 'Lut', 'Nylon', 'Polyester', 'Satin', 'Silk', 'Suede', 'Terrycloth', 'Velvet', 'Viscose', 'Wool']
-    
-    def class_to_index(self, className):
-        return self.classes.index(className)
+    def class_to_index(className):
+        return classes.index(className)
 
-    def index_to_class(self, index):
+    def index_to_class(index):
         return self.classes[index]
+
+    def get_classes():
+        return classes
 
     def get_config():
         with open('config.json') as file:
             config = json.load(file)
             return config
+    
+    def is_number(s):
+        try:
+            float(s)
+            return True
+        except ValueError:
+            return False
