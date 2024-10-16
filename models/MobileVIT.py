52fb424a389f06479ab4d2db9f5072ca4504ea80
MayankJatav <mayankjatav7898@gmail.com>
2024-10-16 16:59:40 +0530

    Modified code for loading from saved model

--- models/MobileVIT.py
+++ models/MobileVIT.py
@@ -4,8 +4,10 @@ from transformers import MobileViTImageProcessor, MobileViTForImageClassificatio
 class MobileVIT(torch.nn.Module):
     def __init__(self):
         super(MobileVIT, self).__init__()
-        self.feature_extractor = MobileViTImageProcessor.from_pretrained("apple/mobilevit-small")
-        self.model = MobileViTForImageClassification.from_pretrained("apple/mobilevit-small")
+        # self.feature_extractor = MobileViTImageProcessor.from_pretrained("apple/mobilevit-small")
+        # self.model = MobileViTForImageClassification.from_pretrained("apple/mobilevit-small")
+        self.feature_extractor = MobileViTFeatureExtractor.from_pretrained("./cache/Tokenizer/MobileViTModel")
+        self.model = MobileViTForImageClassification.from_pretrained("./cache/model/MobileViTModel")
         self.model.classifier = torch.nn.Sequential(
             torch.nn.Linear(640, 21),
             torch.nn.Softmax(),
@@ -13,5 +15,6 @@ class MobileVIT(torch.nn.Module):
 
     def forward(self, input):
         inputs = self.feature_extractor(images=input, return_tensors="pt")
+        inputs = {key: value.to(device, non_blocking=True) for key, value in inputs.items()}
         outputs = self.model(**inputs)
         return outputs.logits
