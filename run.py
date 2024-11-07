6eb4fb53c1b5fdaf30a81d8b90e53946eb35bf9f
MayankJatav <mayankjatav7898@gmail.com>
2024-11-08 01:14:23 +0530

    Added Code For Research Paper "TextileNet: A Deep Learning Approach for Textile Fabric Material Identification from OCT and Macro Images"

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import json
 import dataloader
 from models.DeitModel import DeiTModel
@@ -44,12 +45,11 @@ if __name__ == '__main__':
         dataset = dataloader.FabricOCTDataset(dataset_path, transform)
     else:
         assert False, "Dataset name should be either FabricsDataset or FabricsOCTDataset"
-    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
+    train_dataset, _, val_dataset = random_split(dataset, [train_size, 0.98, val_size])
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
     loss_fn = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(train_model.parameters(), lr=learning_rate)
     
     # if model_name == "mobilenet":
     #     train_model = MobileNetModel(pretrained=True)
@@ -65,54 +65,17 @@ if __name__ == '__main__':
     #     print("Loading Saved Weights:", saved_weights)
     #     train_model.load_state_dict(torch.load(saved_weights).state_dict())
     if model_name == "textilenet":
-        pt_model = "vgg16"
-        train_model = TextileNetModel(pt_model)
-        train_runner = trainer.Trainer(train_model, loss_fn, optimizer, epochs, train_loader, val_loader, 
-                                        log_results_file=f"{current_run_dir}/{pt_model}/{results_file}",
-                                        save_model_file=f"{current_run_dir}/{pt_model}/{save_model_file}"
-                                        )
-        result = train_runner.train()
-        results.save_acc_loss_graph(f"{current_run_dir}/{pt_model}/{results_image}", *result)
-        
-        pt_model = "resnet101"
-        train_model = TextileNetModel(pt_model)
-        train_runner = trainer.Trainer(train_model, loss_fn, optimizer, epochs, train_loader, val_loader, 
-                                        log_results_file=f"{current_run_dir}/{pt_model}/{results_file}",
-                                        save_model_file=f"{current_run_dir}/{pt_model}/{save_model_file}"
-                                        )
-        result = train_runner.train()
-        results.save_acc_loss_graph(f"{current_run_dir}/{pt_model}/{results_image}", *result)
-
-        pt_model = "inceptionv3"
-        train_model = TextileNetModel(pt_model)
-        train_runner = trainer.Trainer(train_model, loss_fn, optimizer, epochs, train_loader, val_loader, 
-                                        log_results_file=f"{current_run_dir}/{pt_model}/{results_file}",
-                                        save_model_file=f"{current_run_dir}/{pt_model}/{save_model_file}"
-                                        )
-        result = train_runner.train()
-        results.save_acc_loss_graph(f"{current_run_dir}/{pt_model}/{results_image}", *result)
-
-        pt_model = "mobilenetv2"
-        train_model = TextileNetModel(pt_model)
-        train_runner = trainer.Trainer(train_model, loss_fn, optimizer, epochs, train_loader, val_loader, 
-                                        log_results_file=f"{current_run_dir}/{pt_model}/{results_file}",
-                                        save_model_file=f"{current_run_dir}/{pt_model}/{save_model_file}"
-                                        )
-        result = train_runner.train()
-        results.save_acc_loss_graph(f"{current_run_dir}/{pt_model}/{results_image}", *result)
-
-        pt_model = "convnextbase"
-        train_model = TextileNetModel(pt_model)
-        train_runner = trainer.Trainer(train_model, loss_fn, optimizer, epochs, train_loader, val_loader, 
-                                        log_results_file=f"{current_run_dir}/{pt_model}/{results_file}",
-                                        save_model_file=f"{current_run_dir}/{pt_model}/{save_model_file}"
-                                        )
-        result = train_runner.train()
-        results.save_acc_loss_graph(f"{current_run_dir}/{pt_model}/{results_image}", *result)
-
-    # if model_name == "textilenet_resnet101":
-    # if model_name == "textilenet_inceptionv3":
-    # if model_name == "textilenet_mobilenetv2":
-    # if model_name == "textilenet_convnextbase":
+        pt_model = ["vgg16", "resnet101", "inceptionv3", "mobilenetv2", "convnextbase"]
+        for model in pt_model:
+            train_model = TextileNetModel(model)
+            optimizer = torch.optim.Adam(train_model.parameters(), lr=learning_rate)
+            if not os.path.exists(f"{current_run_dir}/{model}"):
+                os.makedirs(f"{current_run_dir}/{model}")
+            train_runner = trainer.Trainer(train_model, loss_fn, optimizer, epochs, train_loader, val_loader, 
+                                            log_results_file=f"{current_run_dir}/{model}/{results_file}",
+                                            save_model_file=f"{current_run_dir}/{model}/{save_model_file}"
+                                            )
+            result = train_runner.train()
+            results.save_acc_loss_graph(f"{current_run_dir}/{model}/{results_image}", *result)
     # print(train_model)
     # summary(train_model, (3, model_input_shape[0], model_input_shape[1]))
