b23ad7b1b5e04ac70f195682c3e9d210789c434c
MayankJatav <mayankjatav7898@gmail.com>
2024-11-20 01:12:45 +0530

    Modified Code for three Classes

--- models/MobileVIT.py
+++ models/MobileVIT.py
@@ -10,12 +10,12 @@ class MobileVIT(torch.nn.Module):
         self.feature_extractor = MobileViTFeatureExtractor.from_pretrained("./cache/Tokenizer/MobileViTModel")
         self.model = MobileViTForImageClassification.from_pretrained("./cache/model/MobileViTModel")
         self.model.classifier = torch.nn.Sequential(
-            torch.nn.Linear(640, 5),
+            torch.nn.Linear(640, 3),
             torch.nn.Softmax()
         )
 
     def forward(self, input):
         inputs = self.feature_extractor(images=input, return_tensors="pt")
-        inputs = {key: value.to(self.device, non_blocking=True) for key, value in inputs.items()}
+        # inputs = {key: value.to(self.device, non_blocking=True) for key, value in inputs.items()}
         outputs = self.model(**inputs)
         return outputs.logits
