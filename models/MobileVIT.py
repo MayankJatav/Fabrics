086662898d9d693af68bb54c2729e882fc8c7ec3
MayankJatav <mayankjatav7898@gmail.com>
2024-11-20 01:30:53 +0530

    Uncommented Code For Moving inputs to device

--- models/MobileVIT.py
+++ models/MobileVIT.py
@@ -16,6 +16,6 @@ class MobileVIT(torch.nn.Module):
 
     def forward(self, input):
         inputs = self.feature_extractor(images=input, return_tensors="pt")
-        # inputs = {key: value.to(self.device, non_blocking=True) for key, value in inputs.items()}
+        inputs = {key: value.to(self.device, non_blocking=True) for key, value in inputs.items()}
         outputs = self.model(**inputs)
         return outputs.logits
