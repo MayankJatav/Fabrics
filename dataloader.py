c18fae43ee44c6da8d020246ea314e6d403997a1
MayankJatav <mayankjatav7898@gmail.com>
2024-12-05 02:20:54 +0530

    Corrected Dataset Code

--- dataloader.py
+++ dataloader.py
@@ -8,8 +8,9 @@ import cv2
 import numpy as np
 
 class FabricDataset(Dataset):
-    def __init__(self, path, transform=None):
+    def __init__(self, path, transform=None, preprocess=False):
         # Initialization Code Here
+        self.preprocess = preprocess
         self.transform = transform
         self.files = glob.glob(path+"/**/**/*.png", recursive=True)
         self.files = [s.replace("\\", "/") for s in self.files]
@@ -21,6 +22,8 @@ class FabricDataset(Dataset):
     def __getitem__(self, idx):
         # Return the data at index idx
         image = Image.open(self.files[idx])
+        if self.preprocess:
+            image = self.preprocess_image(image)
         if self.transform:
             image = self.transform(image)
         image = image / torch.max(image)
@@ -30,30 +33,8 @@ class FabricDataset(Dataset):
         label = torch.Tensor(label)
         return image, label
 
-class FabricOCTDataset(Dataset):
-    def __init__(self, path, transform=None):
-        # Initialization Code Here
-        self.transform = transform
-        self.files = glob.glob(path+"/**/**/*.png", recursive=True)
-        self.files = [s.replace("\\", "/") for s in self.files]
-    
-    def __len__(self):
-        # Reuturn  the length
-        return len(self.files)
-
-    def __getitem__(self, idx):
-        # Return the data at index idx
-        image = np.array(Image.open(self.files[idx]))
-        image = self.preprocess_image(image)
-        if self.transform:
-            image = self.transform(image).float()
-        label = [0, 0, 0]
-        className = self.files[idx].split('/')[-3]
-        label[utils.class_to_index(className)] = 1
-        label = torch.Tensor(label)
-        return image, label
-    
     def preprocess_image(self, image):
+        image = np.array(image)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
         image = clahe.apply(image)
