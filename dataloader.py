6809acaab5a4f12aa172de56272c824dff379fa1
MayankJatav <mayankjatav7898@gmail.com>
2024-12-19 16:49:03 +0530

    Modified Dataloader Related Code For TextileNet Paper

--- dataloader.py
+++ dataloader.py
@@ -21,6 +21,7 @@ class FabricDataset(Dataset):
     def __getitem__(self, idx):
         # Return the data at index idx
         image = Image.open(self.files[idx])
+        image = self.preprocess_image(image)
         if self.transform:
             image = self.transform(image)
         image = image / torch.max(image)
@@ -29,6 +30,17 @@ class FabricDataset(Dataset):
         label[utils.class_to_index(className)] = 1
         label = torch.Tensor(label)
         return image, label
+    
+    def preprocess_image(self, image):
+        image = np.array(image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+        image = clahe.apply(image)
+        sharpening_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]) # Apply the sharpening kernel to the image
+        image = cv2.filter2D(image, -1, sharpening_kernel)
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+        image = image / 255
+        return image
 
 class FabricOCTDataset(Dataset):
     def __init__(self, path, transform=None):
@@ -44,7 +56,6 @@ class FabricOCTDataset(Dataset):
     def __getitem__(self, idx):
         # Return the data at index idx
         image = np.array(Image.open(self.files[idx]))
-        image = self.preprocess_image(image)
         if self.transform:
             image = self.transform(image).float()
         label = [0, 0, 0]
@@ -52,13 +63,3 @@ class FabricOCTDataset(Dataset):
         label[utils.class_to_index(className)] = 1
         label = torch.Tensor(label)
         return image, label
-    
-    def preprocess_image(self, image):
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-        image = clahe.apply(image)
-        sharpening_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]) # Apply the sharpening kernel to the image
-        image = cv2.filter2D(image, -1, sharpening_kernel)
-        image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
-        image = image / 255
-        return image
